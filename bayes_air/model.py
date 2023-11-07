94d33dcaa065233437464e39bf374a990ef270ed
Charles Dawson <charles.dwsn@gmail.com>
2023-11-07 14:37:01 -0500

    Add plate to model for simulating multiple days

--- bayes_air/model.py
+++ bayes_air/model.py
@@ -9,26 +9,25 @@ from bayes_air.types import QueueEntry
 
 
 def air_traffic_network_model(
-    state: NetworkState, T: float = 24.0, delta_t: float = 0.1
+    states: NetworkState, T: float = 24.0, delta_t: float = 0.1
 ):
     """
     Simulate the behavior of an air traffic network.
 
     Args:
-        state: the starting state of the simulation
+        states: the starting states of the simulation (will run an independent
+            simulation from each start state). All states must include the same
+            airports.
         T: the duration of the simulation, in hours
         delta_t: the time resolution of the simulation, in hours
     """
-    # Make a copy of the state so we don't change the input state
-    state = copy.deepcopy(state)
-
     # Define parameters used by the simulation
     runway_use_time_std_dev = 1.0 / 60  # 1 minute
     travel_time_variation = 0.05  # 5% variation in travel time
     turnaround_time_variation = 0.05  # 5% variation in turnaround time
 
     # Sample latent variables for airports
-    airport_codes = state.airports.keys()
+    airport_codes = states[0].airports.keys()
     airport_turnaround_times = {
         code: pyro.sample(f"{code}_mean_turnaround_time", dist.Normal(0.75, 0.25))
         for code in airport_codes
@@ -46,47 +45,54 @@ def air_traffic_network_model(
         if origin != destination
     }
 
-    # Assign the latent variables to the airports
-    for airport in state.airports.values():
-        airport.mean_service_time = airport_service_times[airport.code]
-        airport.runway_use_time_std_dev = runway_use_time_std_dev
-        airport.mean_turnaround_time = airport_turnaround_times[airport.code]
-        airport.turnaround_time_std_dev = (
-            turnaround_time_variation * airport.mean_turnaround_time
-        )
+    # Simulate for each state
+    output_states = []
+    for ind in pyro.plate("days", len(states)):
+        state = states[ind]
 
-    # Simulate the movement of aircraft within the system for a fixed period of time
-    t = 0.0
-    for _ in pyro.markov(range(int(T // delta_t))):
-        # Update the current time
-        t += delta_t
-
-        # All parked aircraft that are ready to turnaround get serviced
+        # Assign the latent variables to the airports
         for airport in state.airports.values():
-            airport.update_available_aircraft(t)
-
-        # All flights that are able to depart get moved to the runway queue at their
-        # origin airport
-        ready_to_depart_flights, ready_times = state.pop_ready_to_depart_flights(t)
-        for flight, ready_time in zip(ready_to_depart_flights, ready_times):
-            queue_entry = QueueEntry(flight=flight, queue_start_time=ready_time)
-            state.airports[flight.origin].runway_queue.append(queue_entry)
-
-        # All flights that are using the runway get serviced
-        for airport in state.airports.values():
-            departed_flights, landing_flights = airport.update_runway_queue(t)
-
-            # Departing flights get added to the in-transit list, while landed flights
-            # get added to the completed list
-            state.add_in_transit_flights(
-                departed_flights, travel_times, travel_time_variation
+            airport.mean_service_time = airport_service_times[airport.code]
+            airport.runway_use_time_std_dev = runway_use_time_std_dev
+            airport.mean_turnaround_time = airport_turnaround_times[airport.code]
+            airport.turnaround_time_std_dev = (
+                turnaround_time_variation * airport.mean_turnaround_time
             )
-            state.add_completed_flights(landing_flights)
 
-        # All flights that are in transit get moved to the runway queue at their
-        # destination airport, if enough time has elapsed
-        state.update_in_transit_flights(t)
+        # Simulate the movement of aircraft within the system for a fixed period of time
+        t = 0.0
+        for _ in pyro.markov(range(int(T // delta_t))):
+            # Update the current time
+            t += delta_t
 
-    # Once we're done, return the state (this will include the actual arrival/departure
-    # times for each aircraft)
-    return state
+            # All parked aircraft that are ready to turnaround get serviced
+            for airport in state.airports.values():
+                airport.update_available_aircraft(t)
+
+            # All flights that are able to depart get moved to the runway queue at their
+            # origin airport
+            ready_to_depart_flights, ready_times = state.pop_ready_to_depart_flights(t)
+            for flight, ready_time in zip(ready_to_depart_flights, ready_times):
+                queue_entry = QueueEntry(flight=flight, queue_start_time=ready_time)
+                state.airports[flight.origin].runway_queue.append(queue_entry)
+
+            # All flights that are using the runway get serviced
+            for airport in state.airports.values():
+                departed_flights, landing_flights = airport.update_runway_queue(t)
+
+                # Departing flights get added to the in-transit list, while landed flights
+                # get added to the completed list
+                state.add_in_transit_flights(
+                    departed_flights, travel_times, travel_time_variation
+                )
+                state.add_completed_flights(landing_flights)
+
+            # All flights that are in transit get moved to the runway queue at their
+            # destination airport, if enough time has elapsed
+            state.update_in_transit_flights(t)
+
+        # Once we're done, return the state (this will include the actual arrival/departure
+        # times for each aircraft)
+        output_states.append(state)
+
+    return output_states
--- scripts/two_airport_network.py
+++ scripts/two_airport_network.py
@@ -19,13 +19,12 @@ def main():
             "flight_number": ["F1", "F1"],
             "origin_airport": ["A1", "A2"],
             "destination_airport": ["A2", "A1"],
-            "scheduled_departure_time": [torch.tensor(0.0), torch.tensor(1.0)],
+            "scheduled_departure_time": [torch.tensor(0.0), torch.tensor(1.5)],
             "scheduled_arrival_time": [torch.tensor(1.0), torch.tensor(3.0)],
             "actual_departure_time": [None, None],
             "actual_arrival_time": [None, None],
         }
     )
-
     flights, airports = parse_schedule(schedule)
     airports[0].available_aircraft.append(torch.tensor(0.0))
     state = NetworkState(
@@ -33,14 +32,24 @@ def main():
         pending_flights=flights,
     )
 
+    # Render the model to see what the structure is
     model_graph = pyro.render_model(
         air_traffic_network_model,
-        model_args=(state, 5.0, 0.1),
+        model_args=([state, state], 5.0, 0.1),
         render_params=True,
         render_distributions=True,
     )
-    model_graph.render("tmp/two_airport_one_aircraft", view=True)
-    
+    model_graph.render("tmp/two_airport_one_aircraft")
+
+    # Create an autoguide for the model
+    auto_guide = pyro.infer.autoguide.AutoMultivariateNormal(air_traffic_network_model)
+    guide_graph = pyro.render_model(
+        auto_guide,
+        model_args=([state, state], 5.0, 0.1),
+        render_params=True,
+        render_distributions=True,
+    )
+    guide_graph.render("tmp/two_airport_one_aircraft_guide")
 
 
 if __name__ == "__main__":
